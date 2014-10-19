07466fa922b69fb4edb9b676cf28fdc85ba3d876
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-10-20 01:33:32 +0900

    hopsize should not be optional
    shorten name

--- src/bridge.jl
+++ src/bridge.jl
@@ -175,11 +175,11 @@ end
 # freqt performs frequency tranformation on cepstrum. It can be used to
 # convert linear frequency cepstrum to mel frequency cepstrum.
 function freqt(c::Vector{Float64}, order::Int, α::Float64)
-    originalOrder = length(c)-1
+    org_order = length(c)-1
     transformed = zeros(order+1)
     ccall((:freqt, libSPTK), Void,
           (Ptr{Float64}, Int, Ptr{Float64}, Int, Float64),
-          c, originalOrder, transformed, order, α)
+          c, org_order, transformed, order, α)
     return transformed
 end
 
@@ -196,8 +196,7 @@ end
 
 # swipe performs fundamental frequency (f0) estimation based on
 # SWIPE - A Saw-tooth Waveform Inspired Pitch Estimation
-function swipe(x::Vector{Float64}, samplerate::Int;
-               hopsize::Int=80,
+function swipe(x::Vector{Float64}, samplerate::Int, hopsize::Int=80;
                min::Float64=50.0, max::Float64=800.0,
                st::Float64=0.3, otype::Int=1)
     expectedlen = length(x)/hopsize + 1
