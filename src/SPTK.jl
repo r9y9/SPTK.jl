1590b3332d84c00345870647a6b20a5494a2f772
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-11-15 18:44:53 +0900

    cleanup export

--- src/SPTK.jl
+++ src/SPTK.jl
@@ -5,19 +5,37 @@ module SPTK
 # The original SPTK: http://sp-tk.sourceforge.net/
 
 # (Mel-) Cepstrum related analysis
-export mcep, mgcep, uels, fftcep, mfcc
+export
+    mcep,
+    mgcep,
+    uels,
+    fftcep,
+    mfcc,
 
-# Conversions
-export mgc2b, mc2b, b2mc, c2ir, gc2gc, gnorm, ignorm, freqt, mgc2mgc
+    # Conversions
+    mgc2b,
+    mc2b,
+    b2mc,
+    c2ir,
+    gc2gc,
+    gnorm,
+    ignorm,
+    freqt,
+    mgc2mgc,
 
-# F0 estimation
-export swipe
+    # F0 estimation
+    swipe,
 
-# Waveform generation filters
-export mlsadf, mglsadf
+    # Waveform generation filters
+    mlsadf,
+    mglsadf
 
-# Waveform generation filters (more convenient types and methods)
-export MLSADF, MGLSADF, filter!, synthesis_one_frame!, synthesis!
+    # Waveform generation filters (more convenient types and methods)
+    MLSADF,
+    MGLSADF,
+    filter!,
+    synthesis_one_frame!,
+    synthesis!
 
 deps = joinpath(Pkg.dir("SPTK"), "deps", "deps.jl")
 if isfile(deps)
