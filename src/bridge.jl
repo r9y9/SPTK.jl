a9d42924aaa16c2316fe9252066b5fb51195968d
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2014-12-31 01:48:09 +0900

    add matrix version for almost of all functions

--- src/bridge.jl
+++ src/bridge.jl
@@ -301,3 +301,37 @@ end
 
 # see mglsadf.c in original SPTK for this magic allocation
 mglsadf_delay(order::Int, stage::Int) = zeros((order+1)*stage)
+
+# extend functions for matrix input
+for f in [:mcep,
+          :gcep,
+          :mgcep,
+          :uels,
+          :fftcep,
+          :mfcc,
+          :mc2b,
+          :b2mc,
+          :c2ir,
+          :c2ndps,
+          :ndps2c,
+          :gc2gc,
+          :gnorm,
+          :ignorm,
+          :freqt,
+          :mgc2mgc,
+          :mgclsp2sp,
+          ]
+    @eval begin
+        function $f(x::Matrix{Float64}, args...; kargs...)
+            r = $f(x[:, 1], args...; kargs...)
+            ret = Array(eltype(x), size(r, 1), size(x, 2))
+            for i = 1:length(r)
+                @inbounds ret[i, 1] = r[i]
+            end
+            for i = 2:size(x, 2)
+                @inbounds ret[:, i] = $f(x[:, i], args...; kargs...)
+            end
+            ret
+        end
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,17 +5,32 @@ function test_mgcep()
     println("testing: Mel-generalized cesptrum analysis")
     srand(98765)
     dummy_input = rand(1024)
+    dummy_input_mat = repmat(dummy_input, 1, 10)
 
     c = mcep(dummy_input, 20, 0.41)
     @test length(c) == 21
+    cmat = mcep(dummy_input_mat, 20, 0.41)
+    @test size(cmat) == (21, 10)
+
     c = gcep(dummy_input, 20, -1/4)
     @test length(c) == 21
+    cmat = gcep(dummy_input_mat, 20, -1/4)
+    @test size(cmat) == (21, 10)
+
     c = mgcep(dummy_input, 20, 0.41, -1/4)
     @test length(c) == 21
+    cmat = mgcep(dummy_input_mat, 20, 0.41, -1/4)
+    @test size(cmat) == (21, 10)
+
     c = uels(dummy_input, 20)
     @test length(c) == 21
+    cmat = uels(dummy_input_mat, 20)
+    @test size(cmat) == (21, 10)
+
     c = fftcep(dummy_input, 20)
+    cmat = fftcep(dummy_input_mat, 20)
     @test length(c) == 21
+    @test size(cmat) == (21, 10)
 
     # mgcep when gamma = 0, the result of mgcep is corresponds to mcep
     mc = mcep(dummy_input, 20, 0.41)
@@ -35,18 +50,25 @@ function test_mfcc()
     println("testing: MFCC")
     srand(20121)
     dummy = rand(1024)
+    dummy_mat = repmat(dummy, 1, 10)
 
     # By default, c0 is not contained
     cc = mfcc(dummy, 12)
     @test length(cc) == 12
+    ccmat = mfcc(dummy_mat, 12)
+    @test size(ccmat) == (12, 10)
 
     # with c0
     cc = mfcc(dummy, 12; czero=true)
     @test length(cc) == 13
+    ccmat = mfcc(dummy_mat, 12; czero=true)
+    @test size(ccmat) == (13, 10)
 
     # with c0 + power
     cc = mfcc(dummy, 12; czero=true, power=true)
     @test length(cc) == 14
+    ccmat = mfcc(dummy_mat, 12; czero=true, power=true)
+    @test size(ccmat) == (14, 10)
 end
 
 function test_mgcep_conversion()
@@ -57,27 +79,57 @@ function test_mgcep_conversion()
     dummy_sp = abs(fft(dummy_input))
     dummy_logsp = log(dummy_sp)
     dummy_ceps = rand(20)
+    dummy_ceps_mat = repmat(dummy_ceps, 1, 10)
 
     b = mc2b(dummy_ceps, 0.41)
     @test length(b) == length(dummy_ceps)
+    bmat = mc2b(dummy_ceps_mat, 0.41)
+    @test size(bmat) == (length(dummy_ceps), 10)
+
     c = b2mc(dummy_ceps, 0.41)
     @test length(c) == length(dummy_ceps)
+    cmat = b2mc(dummy_ceps_mat, 0.41)
+    @test size(cmat) == (length(dummy_ceps), 10)
+
     c = c2ir(dummy_ceps, 512)
     @test length(c) == 512
+    cmat = c2ir(dummy_ceps_mat, 512)
+    @test size(cmat) == (512, 10)
+
     ndps = c2ndps(dummy_ceps, 512)
     @test length(ndps) == div(512, 2) + 1
+    ndpsmat = c2ndps(dummy_ceps_mat, 512)
+    @test size(ndpsmat) == (div(512, 2) + 1, 10)
+
     c = ndps2c(ndps, 20)
     @test length(c) == 21
+    cmat = ndps2c(repmat(ndps, 1, 10), 20)
+    @test size(cmat) == (21, 10)
+
     c = gc2gc(dummy_ceps, 0.0, 15, -1/4)
     @test length(c) == 16
+    cmat = gc2gc(dummy_ceps_mat, 0.0, 15, -1/4)
+    @test size(cmat) == (16, 10)
+
     c = gnorm(dummy_ceps, -1/4)
     @test length(c) == length(dummy_ceps)
+    cmat = gnorm(dummy_ceps_mat, -1/4)
+    @test size(cmat) == (length(dummy_ceps), 10)
+
     c = ignorm(dummy_ceps, -1/4)
     @test length(c) == length(dummy_ceps)
+    cmat = ignorm(dummy_ceps_mat, -1/4)
+    @test size(cmat) == (length(dummy_ceps), 10)
+
     c = freqt(dummy_ceps, 22, 0.41)
     @test length(c) == 23
+    cmat = freqt(dummy_ceps_mat, 22, 0.41)
+    @test size(cmat) == (23, 10)
+
     c = mgc2mgc(dummy_ceps, 0.41, 0.0, 22, 0.41, -1/4)
     @test length(c) == 23
+    cmat = mgc2mgc(dummy_ceps_mat, 0.41, 0.0, 22, 0.41, -1/4)
+    @test size(cmat) == (23, 10)
 end
 
 function test_f0()
