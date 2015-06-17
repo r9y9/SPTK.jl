5b2b4b475d2c589bcb71f35f58acd429a475b66c
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-06-18 00:31:57 +0900

    use functions instead of let ... end
    this will give more precise line number that some exception occur

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,15 @@ using Base.Test
 
 # Library routines
 
+println("test library routines")
+
 function test_agexp()
-    println("test_agexp")
+    println("-- test_agexp")
     agexp(1, 1, 1)
 end
 
 function test_cholesky()
-    println("test_choleskey")
+    println("-- test_choleskey")
     srand(98765)
     a = rand(10)
     b = copy(a)
@@ -19,24 +21,24 @@ function test_cholesky()
 end
 
 function test_gexp()
-    println("test_gexp")
+    println("-- test_gexp")
     gexp(1, 1)
 end
 
 function test_glog()
-    println("test_glog")
+    println("-- test_glog")
     glog(1, 1)
 end
 
 function test_mseq()
-    println("test_mseq")
+    println("-- test_mseq")
     mseq()
     mseq()
     mseq()
 end
 
 function test_theq()
-    println("test_theq")
+    println("-- test_theq")
     srand(98765)
     t = rand(10)
     h = copy(t)
@@ -47,7 +49,7 @@ function test_theq()
 end
 
 function test_toeplitz()
-    println("test_toeplitz")
+    println("-- test_toeplitz")
     srand(98765)
     a = rand(10)
     b = copy(a)
@@ -66,22 +68,25 @@ test_toeplitz()
 
 # SPTK APIs
 
-let
-    println("testing: Mel-generalized cesptrum analysis")
+function test_mgcep()
+    println("test mel-generalized cepstrum analysis")
     srand(98765)
     dummy_input = rand(1024)
     dummy_input_mat = repmat(dummy_input, 1, 10)
 
+    println("-- test_mcep")
     c = mcep(dummy_input, 20, 0.41)
     @test length(c) == 21
     cmat = mcep(dummy_input_mat, 20, 0.41)
     @test size(cmat) == (21, 10)
 
+    println("-- test_gcep")
     c = gcep(dummy_input, 20, -1/4)
     @test length(c) == 21
     cmat = gcep(dummy_input_mat, 20, -1/4)
     @test size(cmat) == (21, 10)
 
+    println("-- test_mgcep")
     c = mgcep(dummy_input, 20, 0.41, -1/4)
     @test length(c) == 21
     cmat = mgcep(dummy_input_mat, 20, 0.41, -1/4)
@@ -89,64 +94,77 @@ let
 
     # TODO fix in windows
     @unix_only begin
+        println("-- test_uels")
         c = uels(dummy_input, 20)
         @test length(c) == 21
         cmat = uels(dummy_input_mat, 20)
         @test size(cmat) == (21, 10)
     end
 
+    println("-- test_fftcep")
     c = fftcep(dummy_input, 20)
     cmat = fftcep(dummy_input_mat, 20)
     @test length(c) == 21
     @test size(cmat) == (21, 10)
 
+    println("-- test mcep and mgcep consistency")
     # mgcep when gamma = 0, the result of mgcep is corresponds to mcep
     mc = mcep(dummy_input, 20, 0.41)
     mgc = mgcep(dummy_input, 20, 0.41, 0.0)
     @test_approx_eq_eps mc mgc 1.0e-4
 
+    println("-- test_mgclsp2sp")
     sp = mgclsp2sp(dummy_input, 0.41, -1/4, 512)
     @test length(sp) == 256+1
 
     # refs #5
     # change order 20 -> 40
+    println("-- test changing order (ref: issue #5)")
     mcep(dummy_input, 40, 0.41)
     mgcep(dummy_input, 40, 0.41, 0.0)
 end
 
-let
-    println("testing: MFCC")
+test_mgcep()
+
+function test_mfcc()
+    println("test_mfcc")
     srand(20121)
     dummy = rand(1024)
     dummy_mat = repmat(dummy, 1, 10)
 
     # By default, c0 is not contained
+    println("-- test no c0 is expected")
     cc = mfcc(dummy, 12)
     @test length(cc) == 12
     ccmat = mfcc(dummy_mat, 12)
     @test size(ccmat) == (12, 10)
 
     # with c0
+    println("-- test c0 is expected")
     cc = mfcc(dummy, 12; czero=true)
     @test length(cc) == 13
     ccmat = mfcc(dummy_mat, 12; czero=true)
     @test size(ccmat) == (13, 10)
 
     # with c0 + power
+    println("-- test c0 and power are expected")
     cc = mfcc(dummy, 12; czero=true, power=true)
     @test length(cc) == 14
     ccmat = mfcc(dummy_mat, 12; czero=true, power=true)
     @test size(ccmat) == (14, 10)
 
     # with power
+    println("-- test power is expected")
     cc = mfcc(dummy, 12; czero=false, power=true)
     @test length(cc) == 13
     ccmat = mfcc(dummy_mat, 12; czero=false, power=true)
     @test size(ccmat) == (13, 10)
 end
 
-let
-    println("testing: Mel-genearlized cepstrum conversions")
+test_mfcc()
+
+function test_mgcep_conversions()
+    println("test mgcep conversions")
 
     srand(98765)
     dummy_input = rand(1024)
@@ -155,112 +173,133 @@ let
     dummy_ceps = rand(20)
     dummy_ceps_mat = repmat(dummy_ceps, 1, 10)
 
+    println("-- test_mc2b")
     b = mc2b(dummy_ceps, 0.41)
     @test length(b) == length(dummy_ceps)
     bmat = mc2b(dummy_ceps_mat, 0.41)
     @test size(bmat) == (length(dummy_ceps), 10)
 
+    println("-- test_b2mc")
     c = b2mc(dummy_ceps, 0.41)
     @test length(c) == length(dummy_ceps)
     cmat = b2mc(dummy_ceps_mat, 0.41)
     @test size(cmat) == (length(dummy_ceps), 10)
 
+    println("-- test_c2ir")
     c = c2ir(dummy_ceps, 512)
     @test length(c) == 512
     cmat = c2ir(dummy_ceps_mat, 512)
     @test size(cmat) == (512, 10)
 
+    println("-- test_c2ndps")
     ndps = c2ndps(dummy_ceps, 512)
     @test length(ndps) == div(512, 2) + 1
     ndpsmat = c2ndps(dummy_ceps_mat, 512)
     @test size(ndpsmat) == (div(512, 2) + 1, 10)
 
+    println("-- test_ndps2c")
     c = ndps2c(ndps, 20)
     @test length(c) == 21
     cmat = ndps2c(repmat(ndps, 1, 10), 20)
     @test size(cmat) == (21, 10)
 
+    println("-- test_gc2gc")
     c = gc2gc(dummy_ceps, 0.0, 15, -1/4)
     @test length(c) == 16
     cmat = gc2gc(dummy_ceps_mat, 0.0, 15, -1/4)
     @test size(cmat) == (16, 10)
 
+    println("-- test_gnorm")
     c = gnorm(dummy_ceps, -1/4)
     @test length(c) == length(dummy_ceps)
     cmat = gnorm(dummy_ceps_mat, -1/4)
     @test size(cmat) == (length(dummy_ceps), 10)
 
+    println("-- test_ignorm")
     c = ignorm(dummy_ceps, -1/4)
     @test length(c) == length(dummy_ceps)
     cmat = ignorm(dummy_ceps_mat, -1/4)
     @test size(cmat) == (length(dummy_ceps), 10)
 
+    println("-- test_freqt")
     c = freqt(dummy_ceps, 22, 0.41)
     @test length(c) == 23
     cmat = freqt(dummy_ceps_mat, 22, 0.41)
     @test size(cmat) == (23, 10)
 
+    println("-- test_frqtr")
     c = SPTK.frqtr(dummy_ceps, 22, 0.41)
     @test length(c) == 23
     cmat = SPTK.frqtr(dummy_ceps_mat, 22, 0.41)
     @test size(cmat) == (23, 10)
 
+    println("-- test_mgc2mgc")
     c = mgc2mgc(dummy_ceps, 0.41, 0.0, 22, 0.41, -1/4)
     @test length(c) == 23
     cmat = mgc2mgc(dummy_ceps_mat, 0.41, 0.0, 22, 0.41, -1/4)
     @test size(cmat) == (23, 10)
 
+    println("-- test_mgc2sp")
     sp = mgc2sp(dummy_ceps, 0.41, 0.0, 1024)
     @test length(sp) == 1024>>1+1
     spmat = mgc2sp(dummy_ceps_mat, 0.41, 0.0, 1024)
     @test size(spmat) == (1024>>1+1, 10)
 end
 
-let
-    println("testing: LPC")
+test_mgcep_conversions()
 
+function test_lpc()
+    println("test lpc")
     srand(98765)
     dummy_input = rand(1024)
 
+    println("-- test_lpc")
     l = lpc(dummy_input, 20)
     @test length(l) == 21
     @test !any(isnan(l))
 
-    println("testing: LPC conversions")
-
+    println("-- test_lpc2c")
     c = lpc2c(l)
     @test length(c) == length(l)
     @test !any(isnan(c))
 
+    println("-- test_lpc2lsp")
     lsp = lpc2lsp(l, 20)
     @test length(lsp) == 21
     @test !any(isnan(lsp))
 
+    println("-- test_lpc2par")
     par = lpc2par(l)
     @test length(par) == 21
     @test !any(isnan(par))
 
+    println("-- test_lsp2sp")
     sp = lsp2sp(lsp, 1024)
     @test length(sp) == 1024>>1+1
     @test !any(isnan(sp))
 
+    println("-- test_lspcheck")
     try lspcheck(l); catch @assert false; end
 end
 
-let
-    println("testing: F0 estimation")
+test_lpc()
 
+function test_f0()
+    println("test f0 estimatino")
     srand(98765)
     dummy_input = rand(1024)
 
+    println("-- test_swipe")
     f0 = swipe(dummy_input, 16000, 80)
     @test length(f0) == div(length(dummy_input), 80) + 1
     @test !any(isnan(f0))
     @test all(f0 .>= 0)
 end
 
-let
-    println("testing: Waveform generation filters")
+test_f0()
+
+function test_waveform_generation_filters()
+    println("test waveform generation filters")
 
     srand(98765)
     dummy_input = rand(1024)
@@ -272,6 +311,7 @@ let
     pd = 5
     order = length(dummy_ceps)-1
 
+    println("-- test_poledf")
     # poledf
     d = poledf_delay(order)
     for x in dummy_input
@@ -279,6 +319,7 @@ let
         @test !isnan(y)
     end
 
+    println("-- test_lmadf")
     # lmadf
     d = lmadf_delay(order, pd)
     for x in dummy_input
@@ -286,6 +327,7 @@ let
         @test !isnan(y)
     end
 
+    println("-- test_mlsadf")
     # mlsadf
     d = mlsadf_delay(order, pd)
     for x in dummy_input
@@ -293,6 +335,7 @@ let
         @test !isnan(y)
     end
 
+    println("-- test_mglsadf")
     # mlgasdf
     stage = 12
     d = mglsadf_delay(order, stage)
@@ -302,33 +345,42 @@ let
     end
 end
 
-let
-    println("testing: Window functions")
+test_waveform_generation_filters()
 
+function test_window_functions()
+    println("test windows functions")
     srand(98765)
     x = rand(1024)
 
+    println("-- test_blackman")
     y = blackman(length(x))
     @test length(y) == length(x)
     @test !any(isnan(y))
 
+    println("-- test_hamming")
     y = hamming(length(x))
     @test length(y) == length(x)
     @test !any(isnan(y))
 
+    println("-- test_hanning")
     y = hanning(length(x))
     @test length(y) == length(x)
     @test !any(isnan(y))
 
+    println("-- test_bartlett")
     y = bartlett(length(x))
     @test length(y) == length(x)
     @test !any(isnan(y))
 
+    println("-- test_trapezoid")
     y = trapezoid(length(x))
     @test length(y) == length(x)
     @test !any(isnan(y))
 
+    println("-- test_rectangular")
     y = rectangular(length(x))
     @test length(y) == length(x)
     @test !any(isnan(y))
 end
+
+test_window_functions()
